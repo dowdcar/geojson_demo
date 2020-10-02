9bc2aacce75d6832cae1ded5b0206dd4b1bb79e1
dowdcar <72099525+dowdcar@users.noreply.github.com>
2020-10-02 09:19:24 -0500

    update geojson

--- geojson.js
+++ geojson.js
@@ -1,177 +1,93 @@
-// 1. erase everything below this line and paste in your geojson
-// 2. add the following before your pasted content, so that the map
-//    will understand how to find your geojson. See README.md for more info
-//
-//    var json =
-//
-//    See example below:
-
 var json = {
   "type": "FeatureCollection",
   "features": [
     {
       "type": "Feature",
       "properties": {
-        "stroke": "#555555",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "fill": "#555555",
-        "fill-opacity": 0.5,
-        "image": "https://1.bp.blogspot.com/-WbftUEch0t0/X2DQmPdyksI/AAAAAAAAAIs/JHH1hwcqN0IVWsR8KCYabom1fqoweHBFACLcBGAsYHQ/w1684-h1069-p-k-no-nu/hearts.jpg",
-        "title": "Nebraska State Capitol",
-        "description": "Some description about something that happened at the capitol"
-      },
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [
-              -96.70105934143066,
-              40.806971850092815
-            ],
-            [
-              -96.69826984405518,
-              40.806971850092815
-            ],
-            [
-              -96.69826984405518,
-              40.809131953785965
-            ],
-            [
-              -96.70105934143066,
-              40.809131953785965
-            ],
-            [
-              -96.70105934143066,
-              40.806971850092815
-            ]
-          ]
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#ee05ff",
+        "marker-color": "#7e7e7e",
         "marker-size": "medium",
         "marker-symbol": "",
-        "ingredients": "rhubarb, strawberries, egg, milk, butter, so much sugar",
-        "thanks": "thanks to Linnea for giving me a pie",
-        "title": "Starting Point",
-        "image": "https://s3.amazonaws.com/omeka-net/53984/archive/square_thumbnails/e5847940c8baa5e9e50ad71ee468ba0e.jpg?AWSAccessKeyId=AKIAI3ATG3OSQLO5HGKA&Expires=1600905600&Signature=PapYafLVt%2BGfMiI%2F06Ibpay%2FXPk%3D"
+        "title": "Marc's place",
+        "image": "https://lh3.googleusercontent.com/lioKt93VE5X0Emp6Fv8k9fx455MupYJYlSjKy3N7_IaV2XJ6sh4ce4zqfiPFXa3Yy1WTfD0Wj-Li5x0JhtW5-5LfPCyr-YXsEgImTxUYY6uWJxva7ONcCdv6M8Xzqy-R8yx9woGO0hgaBvebDTkM3V3LybxZNdv7D3X77zWTK1ES87EJIZcC7Xeg-R6Z3hpwhBa41xVcAzar-SM03htTo4DcDnBfD7zEEl6m3Ikoisl_uCp2VAOSTf-81qDWPACQauahoJs71dvhP10apvZhgwMp3q7XwginFrqJm41CAC7ir7fSMGyXq76eTp6doojSvFx1rwV31iCibzzTRB7vyiFHJOUKTEGMhopbEPL0Zh3H2xS-ek8sp4SRiZ_e7Xdd7S_OHsEZmpyM8foPQ_Dfy3jLKM6QbhWpKXYUKe66wsnCn8MrCnjgzSxjNURMJbS2wH6ma8WhIicOKOOBbBzZWsNVGtPCXynCgn__GmQrbV5OeEzB_s8lFXXlLSP-D7XMkcL-Cv7EQJhOZEErum-aOlrrx9qwlcHuH3t94Y9CxtPjMsf4NZ0QUeha8urdplLpAT4anAsM_fwfOtkerQN5dMzsmHsqucpdI5gw8oTl7CtaAX49tPLY_EcQAXoZe7Mx4-QsJSN7klR8dwtP8fSBhmTuxHXbgg2Exe16QBZIguj_WF55KVUhQW30KElI=w528-h937-no?authuser=0",
+        "description": "Location of the last Friendsgiving, November 23rd, 2019"
       },
       "geometry": {
         "type": "Point",
         "coordinates": [
-          -96.70299053192137,
-          40.81015513625622
+          -96.70853734016418,
+          40.826008387727704
         ]
       }
     },
     {
       "type": "Feature",
       "properties": {
-        "stroke": "#ff5100",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "title": "Path to the library",
-        "image": "https://1.bp.blogspot.com/-WbftUEch0t0/X2DQmPdyksI/AAAAAAAAAIs/JHH1hwcqN0IVWsR8KCYabom1fqoweHBFACLcBGAsYHQ/w1684-h1069-p-k-no-nu/hearts.jpg"
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "My house",
+        "image": "https://lh3.googleusercontent.com/YbBzlT_JN1C6knZ2hkD10Rf4PqYIKPhBucVVtbhAEdrnqit_M-pEGLfuD1a6-e61WJcRUbfhOixujtajgYBKtDnnhGPSCvDTdFtGrqiW8KQwwmdMLphj26NmZ0e-Wy28WouUPX1tkj_todAnXUaay_e0m0fIdioed5PRRmgyG4g07soiYLEIRFtLkc9o9Gw4_g9rEl-YlitNcVhD9DZslYdf7__cyI7PpN5QF17QhkuGBgweYXAGRIfT4di6Ak6NpHxc2Dmcc0POK-rH0lr-PzzopdKfgP9yZ1tVjlh5ZPE8auOB_xHuRd4hvwyPx5NsaDCNx_tzCA1F4pqwoyVAq9Cc-p-T8tYxsd8fkSPZScsKUQr7paFst4ZSW05aEjC-R-CU-V-hm4CxpjJSHuT5sJp-PWlFl0fOc3HplEQpapC4nffx2zDn82aaUDZonTE5FgIgS-WX97kMlRyBvG1NmI5dNOUKoXM90JBseOMa4h-cCSHYFtlkDScoNz_n9RR5erP5h6e47Kz5HB6s54y7Dvxz-lueL1v5EHLfEFjLqAs-eIDkYqsntnaBWXBLna0tvd32-OnBA-thWmEm_gu9uazoo0fF3lXftu-6NaTfhXmyMvtHSB95A7NrQrwKakoadu8-CHszbifN5rS1aMi0z8YwODRpFLVzMpFYpY-3rMLuL7WFVZejup-EK3s8=w528-h937-no?authuser=0",
+        "description": "My mom and I seeing my house for the first time."
       },
       "geometry": {
-        "type": "LineString",
+        "type": "Point",
         "coordinates": [
-          [
-            -96.69979333877563,
-            40.80922127986828
-          ],
-          [
-            -96.69986844062804,
-            40.811389430620544
-          ],
-          [
-            -96.6997504234314,
-            40.811405671182825
-          ],
-          [
-            -96.6996967792511,
-            40.81468618325975
-          ],
-          [
-            -96.69984698295592,
-            40.8146943031379
-          ],
-          [
-            -96.69984698295592,
-            40.816837916207085
-          ],
-          [
-            -96.70262575149535,
-            40.81687851427114
-          ]
+          -96.69430017471313,
+          40.80138448884859
         ]
       }
     },
     {
       "type": "Feature",
       "properties": {
-        "stroke": "#d4ff00",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "fill": "#0060ff",
-        "fill-opacity": 0.2,
-        "title": "Love Library",
-        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/189753833@N07/50237618773/in/album-72157715528007683/\" title=\"yawn_connie\"><img src=\"https://live.staticflickr.com/65535/50237618773_c9052299b5_w.jpg\" width=\"300\" height=\"400\" alt=\"yawn_connie\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>",
-        "random": "Some other random field"
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Sunken Gardens",
+        "image": "https://lh3.googleusercontent.com/pw/ACtC-3dWJd8EeVPX8coepYdA0WWB-3IW61BwSVVVnSCBiEV9skRCa1p9wOK-_45sXJRTgy0sXxi_cZfXO1_wYjhD13DdLKVjqdUHcoVSg2-bRfYv0265H05jVmQTe-6jaq-3QY6wIb8jmbr-utBhVr-jWF1ctQ=w703-h937-no?authuser=0",
+        "Title": "The Sunken Garden's Art and Garden show on September 12, 2020."
       },
       "geometry": {
-        "type": "Polygon",
+        "type": "Point",
         "coordinates": [
-          [
-            [
-              -96.70274376869202,
-              40.817040906278805
-            ],
-            [
-              -96.70216441154479,
-              40.81700030831414
-            ],
-            [
-              -96.70187473297119,
-              40.817235776163265
-            ],
-            [
-              -96.70193910598755,
-              40.81753619944697
-            ],
-            [
-              -96.70217514038086,
-              40.81769858984467
-            ],
-            [
-              -96.70218586921692,
-              40.818104564099706
-            ],
-            [
-              -96.70297980308533,
-              40.81808832517719
-            ],
-            [
-              -96.70300126075745,
-              40.817609275175144
-            ],
-            [
-              -96.70323729515076,
-              40.81742252593206
-            ],
-            [
-              -96.70299053192137,
-              40.81704902586876
-            ],
-            [
-              -96.70274376869202,
-              40.817040906278805
-            ]
-          ]
+          -96.68318510055542,
+          40.802147906064356
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Rotary Strolling Garden's memorial fountain",
+        "image": "https://lh3.googleusercontent.com/Zkt9fUzTKlCkSx0FuOkwr8az1Y23g7yA6ESZjFY68bbLA3Eicslo3CmxOSZYb9pJcOFkw1icZTx-jLL-f1GAezfdxGWcMhjl6L4lzIs4T5LS3OuB5XOhvLCegXS_0_ZCeezo39tYLR6lei8AHcB2zUHr6-IZX8eXUGUkep2nZfWy6wkdjXonyJtFOW66AjsG1gTVpo_16b4DymziExrk7gGSLpbY9-e6GT0lip1fUS6-0N42N7pOrYNFjwbG0zfm4DcpwOpE5A7lqlCL5uW4FlajYGfCmY8OUHMnxgCadnnUWSR16cvJ6qxFdRnZoaXqthNYw1tveH926md1zHK7lAAKsL2vnYb43fTOzyP6rmxj7rZBGQfXHTg-CC8jdx-3vZh84fA_ScJ2lpyKgxb476OhqoljZOPIyjpG_gszrMQ16Ot5YLAU67BqhnuRVDAqf_Y-pGr2iM3mQqlMmlQqtZu7ZC4AQNWGIfKH9TTxfQuPI5wdXBHU32ZR14_HYqGO7nUSNPZ87asz1oMB-wLwWtPpMJEaXBWWYCH2UxYddqSrtRXoWq-8bo38b96vZmfrNlVOGvGcD3Mn_T15fbit9ETyWUi-MKhmJbhPO-ugPYYPWdEWFrOP_0oSCWoMr-NhPW-MYqqUeT8eWj5mSUejeY9j0chatEibVx-e6A2OV5Le6Zw5nyFqzGUKaqFX=w528-h937-no?authuser=0",
+        "description": "The Rotary Garden's perpetually dry Teacher Memorial fountain"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.68200492858887,
+          40.80174183417036
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Earl May's",
+        "image": "https://lh3.googleusercontent.com/DII_3335Cjwh7vlGbx0qnvw7cdnpBzmdc5ozk8oX-bfXQEGahbgnnjZ9vkiQY5TqlQ91uWUlmAucR3HZYVs8isxfmi8BZBG_FO_vyxR1OXrr815pM2IcS64ML4W7fcgfiFyaeQYCofkwcGP2yqXJFx72p3YUCuWi7oCD8fbMrBH364UeDITyNhJPzF-J01CELDWK9X8yLe4JNxhnyy8g90hTVpgJxLh51CKAQFMqUdkhEEhEBZJ5zHLrIg2dXWuUOXZ4gihj2AV5GN-FW_Mzg3hcCRZFFtr1q7ls1D1hzIGLddRjrqsd6qhKU8kRDnWaDcuNIxsq-SLSdpCQP9PWM4GWw2pBp1GadIeVTKSwaGHRkRtubcGMx7mIyLeJXJ4_eB66W0UaZ7oLZtNxozivzkJchNu_wBldx6BErH5uBVh88398doWpyzEuvGLY5ecuMngWOu69zNFPu8ntNNGtqW8RsGNbuwiXRMFteOSpUCJomvaVVjei5Tf8PQhkYxFyEV9Yp36rZPCl3SxK1LUkwNaVnsCAwxCdenG0ww5xKEBpMdDVp9b-baL-nyZb22NgN_0DFssqVp9aKFBKt8uf-ZShVlAtYSW4FW3JffOh_rCTjqdOXJ3hfqrXzTI91C2-DwL0w8TB1FBCIlk5hOEblFvC1LlfQwzj02emH9JKFNO6FVaMhUnsIclXiX5j=w528-h937-no?authuser=0",
+        "Description": "Me seeing Dr. Shepard at Earl May's for the first time in two months"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.62232041358948,
+          40.812956626563235
         ]
       }
     }
